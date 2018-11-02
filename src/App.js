16f36075c855caeb12dd8583a229c38dbda6895e
Gabriel Lomba <gabriellomba@hotmail.com>
2018-11-02 12:44:46 -0300

    Build UI and simple state management to process the SRT files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,66 @@
 import React, { Component } from 'react';
+
 import './App.css';
+import { processTimingChange } from './SrtFileService';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      srtFileData: null,
+      timingChange: 0,
+      error: ''
+    }
+  }
+
+  handleChosenFile = (file) => {
+    this.setState({ error: null });
+
+    const fileReader = new FileReader();
+    fileReader.onloadend = () => {
+      this.setState({ srlFileData: fileReader.result });
+    }
+    fileReader.onerror = error => {
+      this.setState({ error });
+    }
+
+    fileReader.readAsText(file);
+  }
+
+  processDataIfValid = () => {
+    if (this.state.srtFileData) {
+      processTimingChange(this.state.srtFileData, this.state.timingChange);
+    } else {
+      this.setState({ error: "Please submit a SRT file before applying!" });
+    }
+  }
+
   render() {
+    const { error } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
+        <header>
           <p>
-            Simple app that adjusts subtitles timing in SRL files.
+            Simple app that adjusts subtitles timing in SRT files.
           </p>
         </header>
         <div className="App-content">
+
           <div className="App-input">
-            <input type="file" />
-            <textarea />
+            <label>Select the SRL file:</label>
+            <input type="file" accept=".srt" onChange={ev => this.handleChosenFile(ev.target.files[0])} />
           </div>
+
           <div className="App-change">
             <label>Timing change applied</label>
-            <input type="number" />
+            <input type="number" onChange={value => this.setState({ timingChange: value })} />
+            <button onClick={this.processDataIfValid}>Apply!</button>
+
+            {error ? <p className="App-error">{error}</p> : null}
           </div>
-          <div className="App-output">
-            <input type="file" />
-            <textarea />
-          </div>
+
         </div>
       </div>
     );
