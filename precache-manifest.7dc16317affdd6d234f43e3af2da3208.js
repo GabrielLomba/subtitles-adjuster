3021f86418a3ec6860d71123efceeef50534b36d
Gabriel Lomba <gabriellomba@hotmail.com>
2018-11-02 15:55:22 -0300

    Updates

---
+++ precache-manifest.7dc16317affdd6d234f43e3af2da3208.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5ce64ccae9a70bac9fc",
+    "url": "/subtitles-adjuster/static/js/runtime~main.b5ce64cc.js"
+  },
+  {
+    "revision": "ef3c47aa3ffb2dc642a3",
+    "url": "/subtitles-adjuster/static/js/main.ef3c47aa.chunk.js"
+  },
+  {
+    "revision": "65960bd25d30b0f8d991",
+    "url": "/subtitles-adjuster/static/js/1.65960bd2.chunk.js"
+  },
+  {
+    "revision": "ef3c47aa3ffb2dc642a3",
+    "url": "/subtitles-adjuster/static/css/main.3011d0e1.chunk.css"
+  },
+  {
+    "revision": "ded3d344e1f0038beeb1dee5df220d3b",
+    "url": "/subtitles-adjuster/index.html"
+  }
+];
